6cfb45e625c9e7bb1baca95c1ac153ea1ccb5930
placerte <pierre.langlois.lacerte@gmail.com>
2025-05-04 16:20:17 -0400

    avancé le GUI

--- ui.py
+++ ui.py
@@ -8,15 +8,21 @@ from defaults import DefaultsFileManagent as dfm
 class QRCodeGeneratorGUI():
 
     root: tk.Tk = tk.Tk()
+    frame_general: tk.LabelFrame = tk.LabelFrame(master=root, text="General", padx=10, pady=10)
     
     url_var: tk.StringVar = tk.StringVar()
     title_var: tk.StringVar = tk.StringVar()
     output_dir_var: tk.StringVar = tk.StringVar(value=dfm.OUTPUT_DIR_PATH)
     print_title_var: tk.BooleanVar = tk.BooleanVar(value=True)
 
+    def build_layout(self):
+
+        self.frame_general.pack(padx=10, pady=10, fill="both", expand=True)
+
     def launch(self):
 
         self.root.title("QR code generator")
+        self.build_layout()
 
         self.build_url_widget()
         self.build_title_widget()
@@ -27,33 +33,33 @@ class QRCodeGeneratorGUI():
 
     def build_title_widget(self):
         # Title Label
-        label_title: ttk.Label = ttk.Label(master=self.root, text="QR code title (optional):")
+        label_title: ttk.Label = ttk.Label(master=self.frame_general, text="QR code title (optional):")
         label_title.pack(padx=dui.PADX, pady=dui.PADY, anchor="w")
 
         # Title entry box
-        title_entry: ttk.Entry = ttk.Entry(master=self.root, textvariable=self.title_var, width=dui.WIDTH)
+        title_entry: ttk.Entry = ttk.Entry(master=self.frame_general, textvariable=self.title_var, width=dui.WIDTH)
         title_entry.pack(padx=dui.PADX, pady=dui.PADY, anchor="w")
 
         
     def build_url_widget(self):
         # URL label
-        label_url: ttk.Label = ttk.Label(master=self.root, text="URL:")
+        label_url: ttk.Label = ttk.Label(master=self.frame_general, text="URL:")
         label_url.pack(padx=dui.PADX, pady=dui.PADY, anchor="w")
 
         # URL Entry
-        url_entry: ttk.Entry = ttk.Entry(master=self.root, textvariable=self.url_var, width=dui.WIDTH)
+        url_entry: ttk.Entry = ttk.Entry(master=self.frame_general, textvariable=self.url_var, width=dui.WIDTH)
         url_entry.pack(padx=dui.PADX, pady=dui.PADY, anchor="w")
 
     def build_output_dir_widget(self):
         # Output directory label
-        label_output_dir: ttk.Label = ttk.Label(master=self.root, text="Output directory:")
+        label_output_dir: ttk.Label = ttk.Label(master=self.frame_general, text="Output directory:")
         label_output_dir.pack(padx=dui.PADX, pady=dui.PADY, anchor="w")
 
         # Output directory Entry
-        output_dir_entry: ttk.Entry = ttk.Entry(master=self.root, textvariable=self.output_dir_var, width=dui.WIDTH)
+        output_dir_entry: ttk.Entry = ttk.Entry(master=self.frame_general, textvariable=self.output_dir_var, width=dui.WIDTH)
         output_dir_entry.pack(padx=dui.PADX, pady=dui.PADY, anchor="w")
 
     def build_print_title_widget(self):
         # Print title checkbox
-        print_title_check: ttk.Checkbutton = ttk.Checkbutton(master=self.root, text="Print title", variable=self.print_title_var)
+        print_title_check: ttk.Checkbutton = ttk.Checkbutton(master=self.frame_general, text="Print title", variable=self.print_title_var)
         print_title_check.pack(padx=dui.PADX, pady=dui.PADY, anchor="w")
